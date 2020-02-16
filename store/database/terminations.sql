bb23bb216a98bc379db468681ed628586b10fac4
Bimal Poudel <anytizer@bimal.org.np>
2020-02-15 17:34:20 -0700

    Basic setup

--- store/database/terminations.sql
+++ store/database/terminations.sql
@@ -4,5 +4,6 @@ CREATE TABLE "terminations" (
 	"termination_reason"	INTEGER NOT NULL DEFAULT 0,
 	"termination_date"	TEXT NOT NULL DEFAULT '0000-00-00 00:00:00',
 	"termination_by"	TEXT NOT NULL DEFAULT "",
-	PRIMARY KEY("termination_id")
+	PRIMARY KEY("termination_id"),
+    FOREIGN KEY(project_id) REFERENCES projects(project_id)
 );
